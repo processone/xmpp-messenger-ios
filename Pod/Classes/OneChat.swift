e73f71b401936916b88dea4347adac17b22909c9
Paul <pmglemaire@gmail.com>
2015-06-12 15:58:26 +0200

    import xmmp

--- Pod/Classes/OneChat.swift
+++ Pod/Classes/OneChat.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import Security
+import XMPPFramework
 
 typealias XMPPStreamCompletionHandler = (shouldTrustPeer: Bool?) -> Void
 typealias OneChatAuthCompletionHandler = (stream: XMPPStream, error: DDXMLElement?) -> Void
