2bbf48928fda9354b16d8935dbdb9162c414577b
Orkhan Alizade <alizadeorkhan@yahoo.com>
2015-12-11 11:06:32 +0400

    Adding Accept & Decline Buddy Request
    These functions will handle buddy requests. You can use them for accepting or declining buddy requests.

--- Pod/Classes/OneRoster.swift
+++ Pod/Classes/OneRoster.swift
@@ -17,7 +17,7 @@ public class OneRoster: NSObject, NSFetchedResultsControllerDelegate {
 	public var delegate: OneRosterDelegate?
 	public var fetchedResultsControllerVar: NSFetchedResultsController?
 	
-	// MARK: Singleton
+	// MARK: Singletonsen
 	
 	public class var sharedInstance : OneRoster {
 		struct OneRosterSingleton {
@@ -120,6 +120,24 @@ extension OneRoster: XMPPRosterDelegate {
         	presence.addAttributeWithName("from", stringValue: OneChat.sharedInstance.xmppStream?.myJID.bare())
         
 		OneChat.sharedInstance.xmppStream?.sendElement(presence)
+    	}
+    	
+    	public func acceptBuddyRequestFrom(username: String) {
+        	let presence: DDXMLElement = DDXMLElement.elementWithName("presence") as! DDXMLElement
+        	presence.addAttributeWithName("to", stringValue: username)
+        	presence.addAttributeWithName("from", stringValue: OneChat.sharedInstance.xmppStream?.myJID.bare())
+        	presence.addAttributeWithName("type", stringValue: "subscribed")
+        	
+        	OneChat.sharedInstance.xmppStream?.sendElement(presence)
+    	}
+    
+    	public func declineBuddyRequestFrom(username: String) {
+        	let presence: DDXMLElement = DDXMLElement.elementWithName("presence") as! DDXMLElement
+        	presence.addAttributeWithName("to", stringValue: username)
+        	presence.addAttributeWithName("from", stringValue: OneChat.sharedInstance.xmppStream?.myJID.bare())
+        	presence.addAttributeWithName("type", stringValue: "unsubscribed")
+
+        	OneChat.sharedInstance.xmppStream?.sendElement(presence)
     	}
 }
 
